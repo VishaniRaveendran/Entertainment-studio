bdabd917524821e845235978e0b8a3c8b058456b
visha2000 <vishani.2019710@iit.ac.lk>
2021-06-26 21:16:32 +0530

    Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <span onClick={()=>window.scroll(0,0)} className='header'> 🎬 Entertainment Studio 🎥</span>
+    )
+}
+
+export default Header
